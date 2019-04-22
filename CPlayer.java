6d4609a6e20623c1ad97f3e025578ada98c4afe5
ckmarchant <48229599+ckmarchant@users.noreply.github.com>
2019-04-22 13:16:35 -0700

    Add files via upload
    Main changes were to CPlayer. Turn order implemented, need to turn the ID numbers used in the code into actual Domino objects. slight changes to CRandom(made the arraylists public, will probably change to protected later).

---
+++ CPlayer.java
@@ -0,0 +1,49 @@
+import java.util.*;
+public class CPlayer extends CRandom{
+
+   Random r = new Random();
+   int player1Turn = r.nextInt(2);
+   ArrayList<Integer> playedPieces = new ArrayList<Integer>();
+   int choice = -1;
+   public void CPlayer()
+   {
+   
+   }
+
+   public int selectDomino()
+   {
+
+      while(choice == -1)
+      {
+         Scanner userIn = new Scanner(System.in);
+         System.out.println("Select a domino to start using a domino from your hand. Check the table for the IDs of each domino");
+      
+         String line = userIn.nextLine();
+      
+         if(player1Turn == 1)
+         {
+            choice = p1Hand.get(p1Hand.indexOf(Integer.parseInt(line)));
+         }
+         else if(player1Turn == 0)
+         {
+            choice = p2Hand.get(p2Hand.indexOf(Integer.parseInt(line)));
+            
+         }
+         if(choice == -1)
+         {
+            System.out.println("That domino either does not exist or is not currently in your hand");
+         }
+      
+      }
+       if(player1Turn == 1) player1Turn = 0;
+       else player1Turn = 1;
+
+      return choice; 
+   }
+   
+   public void setDomino()
+   {
+      if (playedPieces.isEmpty()) playedPieces.add(choice);
+   }
+
+}
--- CRandom.java
+++ CRandom.java
@@ -2,51 +2,52 @@ import java.util.*;
 
 public class CRandom{
 
-int[] p1Hand = new int[10];
-int[] p2Hand = new int[10];
-int[] remainder = new int[8];
-public void CRandom()
-{  
-   Random r = new Random();
+      ArrayList<Integer> p1Hand = new ArrayList<Integer>();
+      ArrayList<Integer> p2Hand = new ArrayList<Integer>();
+      ArrayList<Integer> remainder = new ArrayList<Integer>();
+   public void CRandom()
+   {  
 
-ArrayList<Integer> dList = new ArrayList<Integer>();
-int i = 1;
-while(i >= 28)
-{
-   dList.add(i);
-}
-Collections.shuffle(dList);
+      ArrayList<Integer> dList = new ArrayList<Integer>();
+      int i = 1;
+      while(i >= 28)
+      {
+         dList.add(i);
+      }
+      Collections.shuffle(dList);
+   
+      for(int j = 0; i < 10; i++)
+      {
+         p1Hand.add(dList.get(i));
+      }
+   
+      for(int j = 10; i < 20; i++)
+      {
+         p1Hand.add(dList.get(i));
+      } 
+   
+      for(int j = 20; i < 28; i++)
+      {
+         p1Hand.add(dList.get(i));
+      }
+   }
 
-for(int j = 0; i < 10; i++)
-{
-   p1Hand[i] = dList.get(i);
-}
 
-for(int j = 10; i < 20; i++)
-{
-   p2Hand[i] = dList.get(i);
-} 
+   public ArrayList<Integer> getHand1()
+   {
+      return p1Hand;
+   }
 
-for(int j = 20; i < 28; i++)
-{
-   remainder[i] = dList.get(i);
-}
-}
+   public ArrayList<Integer> getHand2()
+   {
+      return p2Hand;
+   }
 
 
-public int[] getHand1()
-{
-   return p1Hand;
-}
+   public ArrayList<Integer> getRemainder()
+   {
+      return remainder;
+   }
 
-public int[] getHand2()
-{
-   return p2Hand;
-}
-
-public int[] getRemains()
-{
-   return remainder;
-}
 
 }
